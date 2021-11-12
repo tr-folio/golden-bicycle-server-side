f8888cb60b434f154b5304c8e57527b0089a9e5c
tr-folio <tahrimurmoon@gmail.com>
2021-11-12 12:28:23 +0600

    temporary data removed and all data stored and loaded from mongodb

--- index.js
+++ index.js
@@ -9,69 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const products = [
-    {
-        id: 1,
-        productName: 'City Bike',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        img: 'https://i.ibb.co/dKycf1w/robert-bye-t-G36rv-Ceqng-unsplash.jpg'
-    },
-    {
-        id: 2,
-        productName: 'Road Bike',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        img: 'https://i.ibb.co/dKycf1w/robert-bye-t-G36rv-Ceqng-unsplash.jpg'
-    },
-    {
-        id: 3,
-        productName: 'Touring Bike',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        img: 'https://i.ibb.co/dKycf1w/robert-bye-t-G36rv-Ceqng-unsplash.jpg'
-    },
-    {
-        id: 4,
-        productName: 'Fitness Bike',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        img: 'https://i.ibb.co/dKycf1w/robert-bye-t-G36rv-Ceqng-unsplash.jpg'
-    },
-    {
-        id: 5,
-        productName: 'Hybrid Bike',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        img: 'https://i.ibb.co/dKycf1w/robert-bye-t-G36rv-Ceqng-unsplash.jpg'
-    },
-    {
-        id: 6,
-        productName: 'Mountain Bike',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        img: 'https://i.ibb.co/dKycf1w/robert-bye-t-G36rv-Ceqng-unsplash.jpg'
-    },
-    {
-        id: 7,
-        productName: 'Stunt Bike',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        img: 'https://i.ibb.co/dKycf1w/robert-bye-t-G36rv-Ceqng-unsplash.jpg'
-    },
-    {
-        id: 8,
-        productName: 'Baby Bike',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        img: 'https://i.ibb.co/dKycf1w/robert-bye-t-G36rv-Ceqng-unsplash.jpg'
-    },
-    {
-        id: 9,
-        productName: 'Offroad Bike',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        img: 'https://i.ibb.co/dKycf1w/robert-bye-t-G36rv-Ceqng-unsplash.jpg'
-    },
-    {
-        id: 10,
-        productName: 'Fixed-Gear Bike',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        img: 'https://i.ibb.co/dKycf1w/robert-bye-t-G36rv-Ceqng-unsplash.jpg'
-    }
-]
-
 const reviews = [
     {
         review_id: 1,
@@ -93,35 +30,56 @@ const reviews = [
     },
 ]
 
-// mongodb local database server connection
+// // mongodb local database server connection
 const uri = 'mongodb://127.0.0.1:27017'; // 127.0.0.1 = localhost
 // const uri = 'mongodb://localhost:27017'; // 127.0.0.1 = localhost
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// mongodb atlas database server connection
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.budis.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
     try {
         await client.connect();
         console.log('database connected successfully');
+        // const database = client.db('golden_bicycle');
+        // const productsCollection = database.collection('products');
 
         // GET method for products API
-        app.get('/readproducts', (req, res) => {
-            res.send(products);
+        app.get('/readproducts', async (req, res) => {
+            await client.connect();
+            console.log('database connected successfully');
+            const database = client.db('golden_bicycle');
+            const productsCollection = database.collection('products');
+            const result = await productsCollection.find().toArray();
+            res.send(result);
         });
 
         // GET method for limitted products API
-        app.get('/readLimittedProducts/:limit', (req, res) => {
+        app.get('/readLimittedProducts/:limit', async (req, res) => {
+            await client.connect();
+            console.log('database connected successfully');
+            const database = client.db('golden_bicycle');
+            const productsCollection = database.collection('products');
+            const result = await productsCollection.find().toArray();
             const limit = req.params.limit;
             // console.log(limit);
             const limittedProducts = [];
             for (let i = 0; i < limit; i++) {
-                limittedProducts.push(products[i]);
+                limittedProducts.push(result[i]);
             }
             res.send(limittedProducts);
         });
 
         // GET method for reviews API
-        app.get('/readreviews', (req, res) => {
-            res.send(reviews);
+        app.get('/readreviews', async (req, res) => {
+            await client.connect();
+            console.log('database connected successfully');
+            const database = client.db('golden_bicycle');
+            const reviewsCollection = database.collection('reviews');
+            const result = await reviewsCollection.find().toArray();
+            res.send(result);
         });
     }
     finally {
@@ -130,22 +88,6 @@ async function run() {
 }
 
 run().catch(console.dir);
-
-// mongodb atlas database server connection
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.budis.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// async function run() {
-//     try {
-//         await client.connect();
-//         console.log('database connected successfully');
-//     }
-//     finally {
-//         await client.close();
-//     }
-// }
-
-// run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('running golden bicycle server');
